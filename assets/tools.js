cc9713a76e108a61c34ca56dc91c5d789b6a7b49
SimoneRicco <smnrcc0@gmail.com>
2023-04-14 15:31:52 +0200

    add graphic

--- assets/tools.js
+++ assets/tools.js
@@ -1,13 +1,18 @@
 const multiplo3 = "Fizz";
 const multiplo5 = "Buzz";
+const grid = document.querySelector(".grid");
 for (let i = 1; i <= 100; i++) {
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log(multiplo3+multiplo5);
+    if(i % 15 == 0){
+        // console.log(multiplo3+multiplo5);
+        grid.innerHTML += '<div class="element multiplo15">' + (multiplo3+multiplo5) + '</div>';
     }else if (i % 3 == 0) {
-        console.log(multiplo3);
+        // console.log(multiplo3);
+        grid.innerHTML += '<div class="element multiplo3">' + multiplo3 + '</div>';
     }else if (i % 5 == 0){
-        console.log(multiplo5);
+        // console.log(multiplo5);
+        grid.innerHTML += '<div class="element multiplo5">' + multiplo5 + '</div>';
     }else{
-        console.log(i);
+        // console.log(i);
+        grid.innerHTML += '<div class="element">' + i + '</div>';
     }
 }
